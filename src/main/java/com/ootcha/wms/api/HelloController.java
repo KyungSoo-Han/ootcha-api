ea23e1ec65478f036568e2dd9b13f7dd21294505
kyungsoo han <hanks6160@gmail.com>
2023-05-24 01:42:54 +0900

    test

--- src/main/java/com/ootcha/wms/api/HelloController.java
+++ src/main/java/com/ootcha/wms/api/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
     @GetMapping
     public String hello(){
 
-        return "Hello-World22-221";
+        return "Hello-World22-22133";
     }
 
     @GetMapping("/test")
